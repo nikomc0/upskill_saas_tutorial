9d3bec1f38b8fc002dd499831a54149acaa4b028
Dominick Del Bosque <dominick@theosfp.com>
2017-01-07 00:17:56 +0000

    Added conditional logic on sign up form.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+  # Get request for / which is our home page
   def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   
+  # Get request for /about which is our about page
   def about
   end
 end
